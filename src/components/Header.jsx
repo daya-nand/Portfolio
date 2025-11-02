beab33fb45bd039d23aafb6a0db846bc6097751a
daya-nand <heyydayanand@gmail.com>
2025-11-02 14:39:45 +0530

    [images path changes public]

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
           
           {/* Left: Logo + Brand */}
           <div className="flex items-center space-x-2">
-            <img className="w-10 h-10 object-contain" src="/logo1.png" alt="Logo" />
+            <img className="w-10 h-10 object-contain" src="public/logo1.png" alt="Logo" />
             <a
               href="#"
               className="text-xl md:text-2xl font-bold text-white tracking-wide transition-colors"
